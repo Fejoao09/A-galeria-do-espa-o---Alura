f13334f7f97c011f1f961e568899f1bf9ba747f2
Fernando <106090383+Fejoao09@users.noreply.github.com>
2023-03-20 16:30:59 -0300

    first commit

---
+++ src/paginas/PaginaInicial/index.jsx
@@ -0,0 +1,32 @@
+import banner from "./banner.png"
+import styles from "./PaginaInicial.module.scss"
+
+import Cabecalho from "componentes/cabecalho";
+import Galeria from "componentes/Galeria";
+import Menu from "componentes/Menu";
+import Populares from "componentes/Populares";
+import Rodape from "componentes/Rodape";
+
+
+export default function PaginaInicial() {
+    return(
+        <>
+        <Cabecalho/>
+        <main>
+            <section className={styles.principal}>
+                <Menu />
+                <div className={styles.principal__imagem}>
+                    <h1>A galeria mais completa do espaço</h1>
+                    <img src={banner} alt="A imagem da Terra vista pelo espaço" />
+                </div>
+            </section>
+            <div className={styles.galeria}>
+                <Galeria />
+                <Populares />
+            </div>
+        </main>
+        <Rodape />
+        
+        </>
+    )
+}
